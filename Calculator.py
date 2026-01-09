5a6d66e719da073f9852c3ec6e2e15b8c2676d57
rohit6566332-dev <rohit6566332@gmail.com>
2026-01-09 13:37:55 +0530

    Calculator updated

--- Calculator.py
+++ Calculator.py
@@ -9,25 +9,25 @@ print("4. Division")
 
 operation = input("Enter choice (1/2/3/4): ")
 
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
 if operation == "1":
     result = num1 + num2
-    print(num1 ,"+", num2, result)
+    print(num1 ,"+", num2, "=", result)
 
 elif operation == "2":
     result = num1 - num2
-    print(num1, "-", num2, result)
+    print(num1, "-", num2, "=", result)
 
 elif operation == "3":
     result = num1 * num2
-    print(num1,"*", num2, result)
+    print(num1,"*", num2,"=", result)
 
 elif operation == "4":
     if num2 != 0:
         result = num1 / num2
-        print(num1,"/", num2, result)
+        print(num1,"/", num2, "=", result)
     else:
         print("Error: Division by zero")
 
